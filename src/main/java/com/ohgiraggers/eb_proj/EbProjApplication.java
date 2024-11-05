b52afa5d8024e6c83d148ce3543be9aa24279acf
yuhyejin <hj_eu@naver.com>
2024-11-05 09:29:58 +0900

    first commit

---
+++ src/main/java/com/ohgiraggers/eb_proj/EbProjApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraggers.eb_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohgiraggers/eb_proj/EbProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraggers.eb_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
